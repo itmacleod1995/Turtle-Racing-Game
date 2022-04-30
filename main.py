221944e9ee2eb339027cb34877a631756980ef62
Ian <itmacleod@email.wm.edu>
2022-04-30 09:28:57 -0400

    created pop window asking user to bet on a turtle

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 
+#Set up pop up box
+bet = screen.textinput(title="Bet", prompt="Who will win?")
+print(bet)
+
+#create turtles
+
 screen.exitonclick()
 
 
